260ddab4f222717cf39d6a71c098af51e20c588b
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2015-09-25 09:49:17 +0200

    RDS/WEB: move searchbox to navigation template

--- themes/freiburg/theme.config.php
+++ themes/freiburg/theme.config.php
@@ -3,7 +3,8 @@ return array(
     'extends' => 'rds',
     'css' => array(
 		//'/css/bootstrap.min.css', // FIXME: We don't need to include it. Inherited from bootstrap theme
-		'screen.css'
+		'screen.css',
+                'freiburg.css',
     ),
     'js' => array(
 		'vendor/modernizr.custom.2.8.3.min.js',
