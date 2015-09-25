ed57c7a4d0951b6160ce251436a240d375ea9cd2
Markus Beh <markus.beh@ub.uni-freiburg.de>
2015-09-25 09:14:51 +0200

    moved methods for print and add to boogback to rds themes

--- themes/rds/js/rds.js
+++ themes/rds/js/rds.js
@@ -16,7 +16,33 @@
           $(val).replaceWith(resultDetails.data.resultCount)
         }
       });
-
     });
     
+    $('.doExportRecord').click(function() {
+      var id = $(this).data('id');
+      $.ajax({
+        url: path + '/Cart/doExport',
+        type:'POST',
+        dataType:'html',
+        data:{doExport: 'Print', cachePolicy: 'Favorite', 'ids[]': id},
+        success:function(data) {
+          var win = window.open();
+          $(win.document.body).html(data);
+          Lightbox.close();
+        },
+        error:function(d,e) {
+          //console.log(d,e); // Error reporting
+        }
+      });
+      return false;
+    });
+    
+    $('.addItemToCart').click(function(){
+      var vufindId = $(this).data('id');
+      var source = vufindId.split("|")[0];
+      var id = vufindId.split("|")[1];
+      removeItemFromCart(id, source);
+      addItemToCart(id, source);
+      return false;
+    });
   });
