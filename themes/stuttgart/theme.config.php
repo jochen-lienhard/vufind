1251a9d7efa55437acbd2c7fb3b6c31d7ef94e10
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2015-12-10 10:36:23 +0100

    RDS: base for stuttgart

---
+++ themes/stuttgart/theme.config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'extends' => 'rds',
+    'css' => array(
+		//'/css/bootstrap.min.css', // FIXME: We don't need to include it. Inherited from bootstrap theme
+    ),
+    'js' => array(
+    ),
+    'favicon' => 'icons/ub-freiburg.ico',
+    'helpers' => array(
+        'invokables' => array(
+            'rdsindexholding' => 'VuFind\View\Helper\Stuttgart\RDSIndexHolding',
+        )
+    )
+
+);
