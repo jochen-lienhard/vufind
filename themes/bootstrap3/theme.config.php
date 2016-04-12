74098f444aa685f8de244bbb19c0aa8b30d4412b
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2016-04-12 11:21:50 +0200

    fix themes css

--- themes/bootstrap3/theme.config.php
+++ themes/bootstrap3/theme.config.php
@@ -7,7 +7,7 @@ return array(
         //'bootstrap-custom.css',
         'compiled.css',
         'vendor/font-awesome.min.css',
-        'vendor/bootstrap-slider.css',
+        'vendor/bootstrap-slider.min.css',
 	'rdsindex.css',
         'print.css:print',
     ),
